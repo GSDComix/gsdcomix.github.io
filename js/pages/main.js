f1963b6ed3f1b95f0488efdaa0d64ddcbae9366c
Stearells <58369471+Stearells@users.noreply.github.com>
2021-10-06 15:07:19 +0300

    navigation script&comics page

--- js/pages/main.js
+++ js/pages/main.js
@@ -1,3 +1,12 @@
+function cnav(e, k) {
+	let el = document.querySelector(k);
+	if(el == null)
+		return;
+	el.style.backgroundColor = "rgba(100, 20, 20, 0.6)";
+	
+	setTimeout(() => {nav(e);}, 50);
+}
+
 function nav(e) {
 	document.location.href = e;
 }
